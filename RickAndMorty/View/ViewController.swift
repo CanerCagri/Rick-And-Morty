a670929132c83ab3cfbbcec77c4db717c5b44ab7
CanerCagri <canerrcagrii@gmail.com>
2022-05-12 11:35:43 +0300

    Mvvm Architecture fixed.

--- RickAndMorty/View/ViewController.swift
+++ RickAndMorty/View/ViewController.swift
@@ -51,22 +51,20 @@ extension ViewController: CharacterViewModelDelegate {
 extension ViewController: UITableViewDataSource , UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
+        return viewModel.heightRowAt()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.characters.count
+        return viewModel.numberOfRowsInSection()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CharacterTableViewCell
         let cellRow = viewModel.characters[indexPath.row]
-        let url = URL(string: cellRow.image ?? "https://i3.wp.com/simpleandseasonal.com/wp-content/uploads/2018/02/Crockpot-Express-E6-Error-Code.png")
-        let processor = DownsamplingImageProcessor(size: CGSize(width: 100, height: 130))
-        
+        let url = URL(string: cellRow.image ?? viewModel.kingFisherTools().0)
+      
         cell.characterImageView.kf.indicatorType = .activity
-        cell.characterImageView.kf.setImage(with: url, placeholder: nil, options: [.processor(processor), .scaleFactor(UIScreen.main.scale), .transition(.fade(2)), .cacheOriginalImage])
-        
+        cell.characterImageView.kf.setImage(with: url, placeholder: nil, options: [.processor(viewModel.kingFisherTools().1), .scaleFactor(UIScreen.main.scale), .transition(.fade(2)), .cacheOriginalImage])
         cell.nameLabel.text = cellRow.name
         cell.genderLabel.text = cellRow.gender
         cell.speciesLabel.text = cellRow.species
--- RickAndMorty/ViewModel/CharacterViewModel.swift
+++ RickAndMorty/ViewModel/CharacterViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Kingfisher
+import UIKit
 
 class CharacterViewModel : CharacterViewModelProtocol {
     var delegate: CharacterViewModelDelegate?
@@ -16,7 +18,30 @@ class CharacterViewModel : CharacterViewModelProtocol {
     func loadData() {
         service.getData { [weak self] character in
             self?.delegate?.handleViewModelOutput(character!)
-
+            
         }
+    }
+    
+    func heightRowAt() -> CGFloat {
+        return 150
+    }
+    
+    func numberOfRowsInSection() -> Int {
+        return characters.count
+    }
+    
+    func kingFisherTools() -> (String, DownsamplingImageProcessor) {
+        let url =  "https://i3.wp.com/simpleandseasonal.com/wp-content/uploads/2018/02/Crockpot-Express-E6-Error-Code.png"
+        let processor = DownsamplingImageProcessor(size: CGSize(width: 100, height: 130))
+
+        func kingFisherUrl() -> String {
+            return url
+        }
+       
+        func kingFisherProcessor() -> DownsamplingImageProcessor {
+            
+            return processor
+        }
+        return (url, processor)
     }
 }
